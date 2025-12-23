6463fd439e40305dc903a7517a6829a67bdf8a94
dinara323 <yakubova.dinaraa@yandex.ru>
2025-12-23 12:50:17 +0300

    commit message

--- config.py
+++
@@ -1,4 +0,0 @@
-open_weather_token = "ccea3e1fe0647c4e42a8ead0f14d667d"
-
-TELEGRAM_TOKEN = "8348095903:AAGOqzNLl1AdJKNxhZ9E4F14nypIS3_cJUU"  
-OPENWEATHER_TOKEN = "ccea3e1fe0647c4e42a8ead0f14d667d" 
